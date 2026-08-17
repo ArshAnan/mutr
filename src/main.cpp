f66614caf3cc700e4a144b6c5b31f7e759bc1af0
ArshAnan <arshanand2524@gmail.com>
2026-08-17 14:50:18 -0400

    Enhance CMake configuration to support optional AddressSanitizer in Debug builds and update main.cpp with detailed socket setup logging. The AddressSanitizer is disabled by default due to known issues on macOS. Refactor socket setup function to include step-by-step logging for better debugging.

--- src/main.cpp
+++ src/main.cpp
@@ -52,51 +52,94 @@ std::string handleDel(const std::vector<std::string>& args) {
 
 std::unordered_map<std::string, std::function<std::string(const std::vector<std::string>&)>> dispatch;
 // Making sure the port is listening to the client
+// int setupServer(int port) {
+//     // Create a socket
+//     int server_fd = socket(AF_INET, SOCK_STREAM, 0); // Asks the kernel to create a socket with IPv4 and TCP
+//     // The kernel simply returns a file descriptor (an integer) to the socket, that we will use to interact with the socket.
+//     if (server_fd < 0) {
+//         std::cerr << "Failed to create a socket" << std::endl;
+//         exit(1);
+//     }
+//     int opt = 1; // 1 means true, 0 means false. We use to this to "enable this option".
+//     // setsockopt takes the pointer to the option, that is why we use &opt.
+
+//     // setsockopt is used to allow the socket to be reused immediately after the program is closed. Without this, the socket will be in a TIME_WAIT state for 2 minutes after the program is closed.
+//     // When we try to start the server again, the socket will be in a TIME_WAIT state, and the new server will not be able to bind to the same port. Will give an error like "Address already in use".
+
+//     // The function setsockopt takes the following arguments:
+//     // 1. The socket file descriptor (server_fd)
+//     // 2. The level of the socket (SOL_SOCKET)
+//     // 3. The option to set (SO_REUSEADDR)
+//     // 4. The pointer to the option (opt)
+//     // 5. The size of the option (sizeof(opt))
+//     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+//         std::cerr << "Failed to set socket options" << std::endl;
+//         close(server_fd);
+//         exit(1);
+//     }
+
+//     struct sockaddr_in address;
+//     memset(&address, 0, sizeof(address)); // Zero out the structure.
+//     address.sin_family = AF_INET; // Set the family to IPv4.
+//     address.sin_port = htons(port); // The port number. The network uses big-endian format, but most CPUs use little-endian format. So, we need to convert it to little-endian format using htons(port). "htons" stands for "host to network short".
+//     address.sin_addr.s_addr = INADDR_ANY; // The IP address of the server. This is a 32-bit integer in network byte order. The use of "INADDR_ANY" means that the server will listen on all IP addresses.
+
+//     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { // Bind the socket to the address and port.
+//         std::cerr << "Failed to bind the socket" << std::endl; // If the binding fails, exit the program.
+//         close(server_fd);
+//         exit(1);
+//     }
+
+//     if (listen(server_fd, 128) < 0) { // Listen for incoming connections. The 128 is the backlog, which is the number of connections that can be queued up.
+//         std::cerr << "Failed to listen to the socket" << std::endl; // If the listening fails, exit the program.
+//         close(server_fd);
+//         exit(1);
+//     }
+
+//     std::cout << "Server is listening on port " << port << std::endl; // Print a message to the console to indicate that the server is listening on the port.
+
+//     return server_fd;
+// }
+
 int setupServer(int port) {
-    // Create a socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0); // Asks the kernel to create a socket with IPv4 and TCP
-    // The kernel simply returns a file descriptor (an integer) to the socket, that we will use to interact with the socket.
+    std::cerr << "Step 1: creating socket" << std::endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         std::cerr << "Failed to create a socket" << std::endl;
         exit(1);
     }
-    int opt = 1; // 1 means true, 0 means false. We use to this to "enable this option".
-    // setsockopt takes the pointer to the option, that is why we use &opt.
+    std::cerr << "Step 2: socket created, fd = " << server_fd << std::endl;
 
-    // setsockopt is used to allow the socket to be reused immediately after the program is closed. Without this, the socket will be in a TIME_WAIT state for 2 minutes after the program is closed.
-    // When we try to start the server again, the socket will be in a TIME_WAIT state, and the new server will not be able to bind to the same port. Will give an error like "Address already in use".
-
-    // The function setsockopt takes the following arguments:
-    // 1. The socket file descriptor (server_fd)
-    // 2. The level of the socket (SOL_SOCKET)
-    // 3. The option to set (SO_REUSEADDR)
-    // 4. The pointer to the option (opt)
-    // 5. The size of the option (sizeof(opt))
+    int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         std::cerr << "Failed to set socket options" << std::endl;
         close(server_fd);
         exit(1);
     }
+    std::cerr << "Step 3: setsockopt done" << std::endl;
 
     struct sockaddr_in address;
-    memset(&address, 0, sizeof(address)); // Zero out the structure.
-    address.sin_family = AF_INET; // Set the family to IPv4.
-    address.sin_port = htons(port); // The port number. The network uses big-endian format, but most CPUs use little-endian format. So, we need to convert it to little-endian format using htons(port). "htons" stands for "host to network short".
-    address.sin_addr.s_addr = INADDR_ANY; // The IP address of the server. This is a 32-bit integer in network byte order. The use of "INADDR_ANY" means that the server will listen on all IP addresses.
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+    std::cerr << "Step 4: address struct filled" << std::endl;
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { // Bind the socket to the address and port.
-        std::cerr << "Failed to bind the socket" << std::endl; // If the binding fails, exit the program.
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "Failed to bind the socket" << std::endl;
         close(server_fd);
         exit(1);
     }
+    std::cerr << "Step 5: bind succeeded" << std::endl;
 
-    if (listen(server_fd, 128) < 0) { // Listen for incoming connections. The 128 is the backlog, which is the number of connections that can be queued up.
-        std::cerr << "Failed to listen to the socket" << std::endl; // If the listening fails, exit the program.
+    if (listen(server_fd, 128) < 0) {
+        std::cerr << "Failed to listen to the socket" << std::endl;
         close(server_fd);
         exit(1);
     }
+    std::cerr << "Step 6: listen succeeded" << std::endl;
 
-    std::cout << "Server is listening on port " << port << std::endl; // Print a message to the console to indicate that the server is listening on the port.
+    std::cout << "Server is listening on port " << port << std::endl;
 
     return server_fd;
 }
@@ -151,7 +194,7 @@ int main() {
     while (true) {
         int client_fd = accept(serverFileDescriptor, (struct sockaddr *)&client_address, &client_address_len);
         if (client_fd < 0) {
-            std::cerr << "Failed to accept the connection" << std::endl;
+            perror("accept");
             continue;
         }
         std::cout << "New Client Connected" << std::endl;
